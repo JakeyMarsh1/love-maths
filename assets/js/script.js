d0a64b9eb262c5156fb4cb0f8196f660fd273a8d
Jakeymarsh1 <Jake.o.marsh@hotmail.co.uk>
2025-08-12 07:55:04 +0100

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+  // Game logic goes here
+}
+
+function checkAnswer() {
+  // Answer checking logic goes here
+}
+
+function CalculateCorrectAnswers() {
+  // Logic to calculate correct answers goes here
+}
+
+function incrementScore() {
+  // Logic to increment the score goes here
+}
+
+function incrementWrongAnswers() {
+  // Logic to increment the wrong answers goes here
+}
+
+function displayAdditionQuestion() {
+  // Logic to display an addition question goes here
+}
+
+function displaySubtractQuestion() {
+  // Logic to display a subtraction question goes here
+}
+
+function displayMultiplyQuestion() {
+  // Logic to display a multiplication question goes here
+}
